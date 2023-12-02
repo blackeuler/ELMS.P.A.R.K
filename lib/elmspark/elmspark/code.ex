982bf8176be72020b3cf3b2689a63e50fcf2d334
Justin Holzmann <jpholzmann@gmail.com>
2023-12-02 17:56:38 -0600

    Change Code to depend on git.

--- lib/elmspark/elmspark/code.ex
+++ lib/elmspark/elmspark/code.ex
@@ -1,10 +1,12 @@
 defmodule ElmSpark.Code do
-  # TODO: Is this meant to be done through the LSP?
-  #   Is this an entire project?
-  #   Part of a project?
-  #   What is its data type?
+  # WIP. Still need a solution for this.
   defstruct [
     :id,
-    :raw
+    :row_start,
+    :column_start,
+    :row_end,
+    :column_end,
+    :git_branch,
+    :git_commit,
   ]
 end
